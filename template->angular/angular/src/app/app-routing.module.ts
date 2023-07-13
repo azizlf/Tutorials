f186df6b100ae6d9b86d40dcc800a82607bc52b9
azizlf <developer.mohamedazizlouifi@gmail.com>
2023-07-13 06:01:21 +0200

    fdsqfdsq

---
+++ template->angular/angular/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+import { RegisterComponent } from './register/register.component';
+
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+
+  {
+    path:"",
+    component:LoginComponent
+  },
+  {
+    path:"register",
+    component:RegisterComponent
+  },
+  {
+    path:"contact",
+    component:ContactComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
